87ad9e4b81b5330b6d1e1b409d7b7ba04fac9fa8
zoolotoui <chag.med.amine@gmail.com>
2024-03-05 11:24:18 +0100

    daily push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 @include('includes.head')
+
 <body class="bg-[#f3f4f6]">
-   
+
  <section>
  <div class="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
   <div class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
